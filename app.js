34ece3af005feb4b7923a24ef8116981ced73586
lucashrossi <lucashrossi@gmail.com>
2023-06-02 00:51:38 -0300

    Second

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 //createApp
 const {createApp} = Vue;
 
+
+const Headerh = {
+
+    props: {
+        tituloh: String,
+        foto: String,
+        total: Number,
+        fecha: Date
+
+    },
+    template: `
+        <h2>{{tituloh}}</h2>
+        <h2>{{total}}</h2>
+        <h4>{{fecha}}</h4>
+        <img :src="foto" :alt="'SinImagen'">
+    `
+
+}
+
 createApp({
+    components: {
+        'headerh': Headerh
+    },
     data() {
         return {
             titulo: "Contador",
-            numero: 0
+            numero: 0,
+            tituloh: "",
+            foto: "./2023.png",
+            total: 0,
+            fecha: 0
            
         }
     },
@@ -14,14 +40,25 @@ createApp({
     },
     methods: {
         calculo() {
+            this.total = 
             this.numero = 10
+            this.tituloh =  "-"
+            this.fecha = Date(this.fecha).toString()
         
         },
         suma() {
-            return this.numero += 1
+            this.total += this.numero 
+            this.numero += 1
+            this.tituloh =  "Sumaste!"
+            this.fecha = Date(this.fecha).toString()
+                
         },
         resta() {
+            this.total -= this.numero
             this.numero -= 1
+            this.tituloh =  "Restaste!"
+            this.fecha = Date(this.fecha).toString()
+             
         }
     },
     computed: {
